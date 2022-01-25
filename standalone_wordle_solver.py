4f494f82d8cc42c8abc47920f6f4bf34912c0be6
Shubh Vachher <shubhvachher@gmail.com>
2022-01-25 20:12:51 +0530

    all working

--- standalone_wordle_solver.py
+++ standalone_wordle_solver.py
@@ -18,7 +18,7 @@ print(dictionary_location, "DICTIONARY WORKING WELL... {} words loaded in memory
 
 # print("robot" in five_letter_english_words)
 
-def wordle_possibilities(word_required=None, unstable_required=None, verbosity=0):
+def wordle_possibilities(word_required=None, unstable_required=None, known_impossibles=None, verbosity=0):
     if not word_required:
         # Try default word. REMOVE THIS LATER AND ALSO ADD DOCSTRING.
         word_required = ['']*5
@@ -32,116 +32,145 @@ def wordle_possibilities(word_required=None, unstable_required=None, verbosity=0
 
         unstable_required = "rb"
 
-    #find-possible-words
+        known_impossibles = "xf"
+    
+    if isinstance(word_required, str):
+        final_word_configuration = []
+        i = 0
+        while i < len(word_required):
+            if word_required[i]=="U":
+                final_word_configuration.append("U")
+            elif word_required[i]=="S":
+                final_word_configuration.append("S"+word_required[i+1])
+                i += 1
+            else:
+                raise("THIS IS AN INVALID ENTRY. READ INSTRUCTIONS.")
+            
+            i += 1
+
+        word_required = final_word_configuration
+
     matching_words = []
 
     for five_letter_english_word in five_letter_english_words:
         failed = False
         failing = True
-        letters = unstable_required[:]  # Create a copy. letters will be changed.
+        letters_required = unstable_required[:]  # Create a copy. letters will be changed.
+    
         for i, letter_code in enumerate(word_required):
+            
             if verbosity>=1:
                 print("_________________________________________________________")
                 print("Starting round for {}\t\t{}. Failed yet? {}".format(letter_code, five_letter_english_word, failed))
+            
             if letter_code[0]=="S":
-                if verbosity>=1:
-                    print("Started S Processing...")
                 
                 letter = letter_code[1]
                 
-                if verbosity>=1:
-                    print("Letter: {}".format(letter))
-                    print(i, "letter_code was: {} word: {}".format(letter_code, five_letter_english_word))
-
                 if five_letter_english_word[i] != letter:
+    
+                    if verbosity>=1:
+                        print("{} is not {}. So, FAILED.".format(five_letter_english_word[i], letter))
+    
                     failed = True
                     break
                 #So, letter == required letter at this position. five_letter_english_word passing for now.
+            
             elif letter_code[0]=="U":
-                if verbosity>=1:
-                    print("Started U Processing...", end=" ")
-                if letters:
-                    if verbosity>=1:
-                        print("Letters Left: {}".format(letters))
-                    letter_found = [False]*len(letters)
-                else:
-                    if verbosity>=1:
-                        print("NO LETTERS LEFT! SKIPPING...")
-                    continue
-                    letter_found = []
-                
-                to_remove = []
-                for j, letter in enumerate(letters):
-                    if letter_found[j]==False:
-                        if five_letter_english_word[i] == letter:
-                            to_remove.append(j)
-                            failing = False  # Shouldn't this be True instead?
-                            letter_found[j] = True
-                            break
-                
-                for j in to_remove:
-                    letters = letters[:j-1]+letters[j+1:]
+                if five_letter_english_word[i] in known_impossibles:
 
-                if verbosity>=1:
-                    print("letter_found, letters remaining, word:", letters, five_letter_english_word[i])        
-                        
-                if failing:
+                    if verbosity>=1:
+                        print("{} found! So, FAILED.".format(five_letter_english_word[i]))
+
                     failed = True
                     break
-                #So, letter == required possible letter at this position. five_letter_english_word passing for now.
+
+                if five_letter_english_word[i] in letters_required:
+                    letters_required = letters_required.replace(five_letter_english_word[i], "")
+
+                    if verbosity>=1:
+                        print("Found {} and new required letters list is {}".format(five_letter_english_word[i], letters_required))
             else:
                 if verbosity>=1:
                     print("ERRRRRRRRRRRORRRR Make sure configuration is correctly defined in code above", letter_code)
         
-        if not failed and len(letters)==0:
+        if not failed and len(letters_required)==0:
             if verbosity>=1:
-                if five_letter_english_words:
-                    print("WORDS SUPER LIKED YOU TODAY! Great Success --------------------------------> {}".format(five_letter_english_word))
-                else:
-                    print("SORRY! I can't find any word in my dictionary list that matches that configuration. Please try giving me a bigger dictionary list to ingest!")
+                print("WORD FOUND! Great Success --------------------------------> {}".format(five_letter_english_word))
+     
             matching_words.append(five_letter_english_word)
-            print(matching_words, "\n\n\n\n\n")
+      
+            if verbosity>=2:
+                print(matching_words, "\n\n\n\n\n")
         
-    print("ALRIGHT ALRIGHT> ALL DONE> POSSIBLE WORDS TO CHOOSE> \n\n", matching_words)
+    print("ALRIGHT ALRIGHT... ALL DONE! WORDS THAT SUPER LIKED YOU TODAY ---> \n\n", matching_words)
     return matching_words
 
 # Getting configuration from the user
 
 def play_wordle():
+    
     print("""
+
 Welcome to Worlde Bot. Please help me help you.
 
 Here are the instructions:
-You will be asked to enter a word configuration soon. Don't worry. Here's what it is.
+You will be asked to enter a word configuration soon. Don't worry. Here's how it's done.
 
 "U" : Unknown Word
 "S" : Known (Stable) Word
 
-Known Words List : Letters that may fill up one of the unknown-word blanks. eg. "rb", "axc".
-
-Example
--------
 So, if we know that 
-- The letters "r" and "b" are in a word. 
-- "o" is definitely letter number 2 and 
-- "t" is definitely letter number 5 then:
+- "o" IS definitely letter number 2 
+- "t" IS definitely letter number 5
+- "r" and "b" are there somewhere in the word
+- "b", "x" and "f" are NOT in the word
 
 Then, 
 
-Word Config will be : U So U U St (Unknown, Stable Letter "O", Unknown, Unknown, "Stable Letter "T")
+Word Config will be : U So U U St (without spaces) (Unknown, Stable Letter "O", Unknown, Unknown, "Stable Letter "T")
+
+And Known Words List : Letters that may fill up one of the unknown-word blanks. eg. "rb", "axc".
+
+And Known Impossibilities List : Letters that are not there in the word.
+___________________________________________________________________________________________________________________________________
 """)
-    verbosity = input("Please enter the verbosity you desire from the robot: ")
+
+    verbosity = int(input("Please enter the VERBOSITY you desire from the robot: "))
 
     while True:
         try:
-            word_configuration = input("Enter Word Configuration : ").strip()
+            word_configuration = input("Word Configuration : ").strip()
             if word_configuration.lower()=="exit":
                 break
-            unknown_variables = input("Enter Known Words List (eg: rbmx) : ")
+                # raise()
 
-            wordle_possibilities(word_configuration, unknown_variables, verbosity)
+            
+            final_word_configuration = []
+            i = 0
+            while i < len(word_configuration):
+                if word_configuration[i]=="U":
+                    final_word_configuration.append("U")
+                elif word_configuration[i]=="S":
+                    final_word_configuration.append("S"+word_configuration[i+1])
+                    i += 1
+                else:
+                    raise("THIS IS AN INVALID ENTRY. READ INSTRUCTIONS.")
+                
+                i += 1
+                
+            unknown_variables = input("Known Words : ")
+
+            known_impossibles = input("Known Impossibles : ")
+
+            print("Starting the Algorithm with inputs: {}, {}, {} and verbosity {}".format(final_word_configuration, 
+                                                                                            unknown_variables,
+                                                                                            known_impossibles,
+                                                                                            verbosity))
+
+            wordle_possibilities(final_word_configuration, unknown_variables, known_impossibles, verbosity)
         except Exception as e:
-            print("ERRRRRRRRRRRRRRRRRRRORRRRRRRRRRRRRRRRRRRRR", e)
+            print("ERRRRRRRRRRRRRRRRRRRORRRRRRRRRRRRRRRRRRRRR", e.message())
             break
     
     print("Buh Byee")
